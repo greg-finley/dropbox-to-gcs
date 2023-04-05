66ad85ee1a4e63b7aa54f0f99278b73b77ada1e4
Greg Finley <gregory.finley@gmail.com>
2023-04-05 09:06:15 -0700

    Read from file system

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mimetypes
 import os
 
 import dropbox
@@ -6,56 +7,82 @@ from google.cloud import storage
 
 load_dotenv()
 
-# Initialize Dropbox client
-dbx = dropbox.Dropbox(os.getenv("ACCESS_TOKEN"))
+# Check MySql if file has been uploaded already
 
-has_more = True
-cursor = None
-num_files = 0
 
-while has_more:
-    if cursor is None:
-        result = dbx.files_list_folder(path="", recursive=True)
-    else:
-        result = dbx.files_list_folder_continue(cursor)
+def print_files_recursively(directory_path):
+    file_count = 0
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isdir(file_path):
+            subdirectory_file_count = print_files_recursively(file_path)
+            file_count += subdirectory_file_count
+        else:
+            content_type = (
+                mimetypes.guess_type(file_path)[0] or "application/octet-stream"
+            )
+            print(
+                f"{file_path.removeprefix('/Users/gregoryfinley/Dropbox/')} - {content_type}"
+            )
+            file_count += 1
+    return file_count
 
-    for entry in result.entries:
-        # Skip folders
-        if isinstance(entry, dropbox.files.FolderMetadata):
-            continue
 
-        # Skip deleted
-        if isinstance(entry, dropbox.files.DeletedMetadata):
-            continue
+directory_path = "/Users/gregoryfinley/Dropbox"
+total_file_count = print_files_recursively(directory_path)
+print(f"Total files: {total_file_count}")
 
-        if not entry.path_lower.endswith(".pdf"):
-            continue
 
-        # if not entry.path_lower.endswith(".tiff"):
-        #     continue
+# # Initialize Dropbox client
+# dbx = dropbox.Dropbox(os.getenv("ACCESS_TOKEN"))
 
-        # Print the file and its folder path
-        print(entry.path_lower)
-        dropbox_file = dbx.files_download(entry.path_display)
-        # Print the file content-type
-        content_type = dropbox_file[1].headers.get("Content-Type")
+# has_more = True
+# cursor = None
+# num_files = 0
 
-        # Create a GCS client object
-        gcs_client = storage.Client()
+# while has_more:
+#     if cursor is None:
+#         result = dbx.files_list_folder(path="", recursive=True)
+#     else:
+#         result = dbx.files_list_folder_continue(cursor)
 
-        # Get a reference to the GCS bucket
-        gcs_bucket = gcs_client.get_bucket("greg-finley-dropbox-backup")
+#     for entry in result.entries:
+#         # Skip folders
+#         if isinstance(entry, dropbox.files.FolderMetadata):
+#             continue
 
-        # Upload the file to GCS
-        gcs_file = gcs_bucket.blob(entry.path_lower)
-        gcs_file.upload_from_string(dropbox_file[1].content, content_type=content_type)
-        num_files += 1
+#         # Skip deleted
+#         if isinstance(entry, dropbox.files.DeletedMetadata):
+#             continue
 
-    # Update cursor
-    cursor = result.cursor
+#         if not entry.path_lower.endswith(".pdf"):
+#             continue
 
-    # Repeat only if there's more to do
-    # has_more = result.has_more
-    has_more = False
+#         # if not entry.path_lower.endswith(".tiff"):
+#         #     continue
 
-print("Total number of files: ", num_files)
+#         # Print the file and its folder path
+#         print(entry.path_lower)
+#         dropbox_file = dbx.files_download(entry.path_display)
+#         # Print the file content-type
+#         content_type = dropbox_file[1].headers.get("Content-Type")
+
+#         # Create a GCS client object
+#         gcs_client = storage.Client()
+
+#         # Get a reference to the GCS bucket
+#         gcs_bucket = gcs_client.get_bucket("greg-finley-dropbox-backup")
+
+#         # Upload the file to GCS
+#         gcs_file = gcs_bucket.blob(entry.path_lower)
+#         gcs_file.upload_from_string(dropbox_file[1].content, content_type=content_type)
+#         num_files += 1
+
+#     # Update cursor
+#     cursor = result.cursor
+
+#     # Repeat only if there's more to do
+#     # has_more = result.has_more
+#     has_more = False
+
+# print("Total number of files: ", num_files)
