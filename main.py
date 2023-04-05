f3ac5564732f51368fdf7992def491415d815ff2
Greg Finley <gregory.finley@gmail.com>
2023-04-05 08:53:32 -0700

    Other version

--- main.py
+++ main.py
@@ -9,35 +9,53 @@ load_dotenv()
 # Initialize Dropbox client
 dbx = dropbox.Dropbox(os.getenv("ACCESS_TOKEN"))
 
-file_count = 0
+has_more = True
+cursor = None
+num_files = 0
 
-
-def list_files(dbx, path="", cursor=None):
-    global file_count
-    # List files in the current folder
+while has_more:
     if cursor is None:
-        result = dbx.files_list_folder(path)
+        result = dbx.files_list_folder(path="", recursive=True)
     else:
         result = dbx.files_list_folder_continue(cursor)
 
-    # Print each file name and update file count
     for entry in result.entries:
-        # if isinstance(entry, dropbox.files.FileMetadata):
-        #     print(entry.path_lower)
-        #     file_count += 1
-
-        # Recursively list files in subfolders
+        # Skip folders
         if isinstance(entry, dropbox.files.FolderMetadata):
-            print(entry.path_lower)
-            list_files(dbx, entry.path_display, None)
+            continue
 
-    # Repeat the process if there are more files
-    if result.has_more:
-        list_files(dbx, path, result.cursor)
+        # Skip deleted
+        if isinstance(entry, dropbox.files.DeletedMetadata):
+            continue
 
+        if not entry.path_lower.endswith(".pdf"):
+            continue
 
-# List all files in Dropbox and count them
-try:
-    list_files(dbx)
-finally:
-    print(f"Total number of files (excluding folders): {file_count}")
+        # if not entry.path_lower.endswith(".tiff"):
+        #     continue
+
+        # Print the file and its folder path
+        print(entry.path_lower)
+        dropbox_file = dbx.files_download(entry.path_display)
+        # Print the file content-type
+        content_type = dropbox_file[1].headers.get("Content-Type")
+
+        # Create a GCS client object
+        gcs_client = storage.Client()
+
+        # Get a reference to the GCS bucket
+        gcs_bucket = gcs_client.get_bucket("greg-finley-dropbox-backup")
+
+        # Upload the file to GCS
+        gcs_file = gcs_bucket.blob(entry.path_lower)
+        gcs_file.upload_from_string(dropbox_file[1].content, content_type=content_type)
+        num_files += 1
+
+    # Update cursor
+    cursor = result.cursor
+
+    # Repeat only if there's more to do
+    # has_more = result.has_more
+    has_more = False
+
+print("Total number of files: ", num_files)
